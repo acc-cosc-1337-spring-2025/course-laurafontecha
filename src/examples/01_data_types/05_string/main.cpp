e895633a07cbc4daeef82d79a564fa8b287844f3
laurafontecha <laura.fontechaleon@g.austincc.edu>
2025-02-11 00:23:24 +0000

    String data type

--- src/examples/01_data_types/05_string/main.cpp
+++ src/examples/01_data_types/05_string/main.cpp
@@ -1,7 +1,12 @@
-
+#include<iostream>
+#include<string>
 //create string variable, initialize and display
+using std::cout;
+using std::string;
 int main()
 {
 
+    string lang="c++";
+    cout<<lang<<"\n";
     return 0;
 }
