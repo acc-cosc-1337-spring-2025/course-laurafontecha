a3e30badc9403bae40e35b4e24b46fcec8d74116
laurafontecha <laura.fontechaleon@g.austincc.edu>
2025-03-09 05:21:57 +0000

    HW 05

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,40 @@
 //add include statements
-
+#include "func.h"
 //add function code here
+using namespace std;
+
+double get_gc_content(const string& dna) 
+{
+    if (dna.empty()) {
+        return 0.0;
+    }
+
+    int gc_count = 0;
+    for (char c : dna) {
+        if (c == 'C' || c == 'G') {
+            gc_count++;
+        }
+    }
+    return static_cast<double>(gc_count) / dna.length();
+}
+
+string get_reverse_string(string dna) {
+    string reversed_dna = "";
+    for (int i = dna.length() - 1; i >= 0; --i) {
+        reversed_dna += dna[i];
+    }
+    return reversed_dna;
+}
+
+string get_dna_complement(string dna) {
+    string reversed_dna = get_reverse_string(dna);
+    for (char& c : reversed_dna) {
+        switch (c) {
+            case 'A': c = 'T'; break;
+            case 'T': c = 'A'; break;
+            case 'C': c = 'G'; break;
+            case 'G': c = 'C'; break;
+        }
+    }
+    return reversed_dna;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,32 @@
+#include "func.h"
+#include<iostream>
+using namespace std;
+int main() {
+    int choice;
+    string dna;
 
-int main() 
-{
+    while (true) {
+        cout << "Menu:\n";
+        cout << "1 - Get GC Content\n";
+        cout << "2 - Get DNA Complement\n";
+        cout << "3 - Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-	return 0;
+        if (choice == 1) {
+            cout << "Enter DNA string: ";
+            cin >> dna;
+            cout << "GC Content: " << get_gc_content(dna) << endl;
+        } else if (choice == 2) {
+            cout << "Enter DNA string: ";
+            cin >> dna;
+            cout << "DNA Complement: " << get_dna_complement(dna) << endl;
+        } else if (choice == 3) {
+            break;
+        } else {
+            cout << "Invalid choice. Please try again.\n";
+        }
+    }
+
+    return 0;
 }
